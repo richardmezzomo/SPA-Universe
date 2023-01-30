3d6ea3d41be33ed085d932878b45afbbe68bb320
richardbmezzomo <richardmezzomo1@gmail.com>
2023-01-30 20:26:11 -0300

    add the concept of SPA

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// const routes = {
+//   "/": "/pages/home.html",
+//   "/universe": "/pages/universe.html",
+//   "/explorer": "/pages/explorer.html",
+// }
+
+
+
+handle()
+
+window.onpopstate = () => handle()
+window.route = () => route()
